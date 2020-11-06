75c9d17103bd2707f35d54f7cff1292a1541381f
Niklas Kiefer <niklas.kiefer@camunda.com>
2020-11-06 14:32:24 +0100

    feat: validate property.type <> property.binding.type
    Closes #9
    Related to https://github.com/camunda/camunda-modeler/issues/1939

---
+++ test/fixtures/invalid-field-type.js
@@ -0,0 +1,61 @@
+export const template = {
+  'name': 'InvalidFieldType',
+  'id': 'com.camunda.example.InvalidFieldType',
+  'appliesTo': [
+    'bpmn:Task'
+  ],
+  'properties': [
+    {
+      'label': 'foo',
+      'type': 'String',
+      'binding': {
+        'type': 'camunda:field',
+        'name': 'foo'
+      }
+    },
+    {
+      'label': 'bar',
+      'type': 'Boolean',
+      'binding': {
+        'type': 'camunda:field',
+        'name': 'bar'
+      }
+    }
+  ]
+};
+
+export const errors = [
+  {
+    'keyword': 'errorMessage',
+    'dataPath': '/properties/1/type',
+    'schemaPath': '#/properties/properties/items/allOf/4/then/properties/type/errorMessage',
+    'params': {
+      'errors': [
+        {
+          'keyword': 'enum',
+          'dataPath': '/properties/1/type',
+          'schemaPath': '#/properties/properties/items/allOf/4/then/properties/type/enum',
+          'params': {
+            'allowedValues': [
+              'String',
+              'Text',
+              'Hidden',
+              'Dropdown'
+            ]
+          },
+          'message': 'should be equal to one of the allowed values'
+        }
+      ]
+    },
+    'message': 'invalid property type "Boolean" for binding type "camunda:field"; must be any of { String, Text, Hidden, Dropdown }'
+  },
+  {
+    'keyword': 'if',
+    'dataPath': '/properties/1',
+    'schemaPath': '#/properties/properties/items/allOf/4/if',
+    'params': {
+      'failingKeyword': 'then'
+    },
+    'message': 'should match "then" schema'
+  }
+];
---
+++ test/fixtures/invalid-output-parameter-type.js
@@ -0,0 +1,52 @@
+export const template = {
+  'name': 'InvalidOutputParameterType',
+  'id': 'com.camunda.example.InvalidOutputParameterType',
+  'appliesTo': [
+    'bpmn:Task'
+  ],
+  'properties': [
+    {
+      'label': 'foo',
+      'type': 'Text',
+      'binding': {
+        'type': 'camunda:outputParameter',
+        'source': 'foo'
+      }
+    }
+  ]
+};
+
+export const errors = [
+  {
+    'keyword': 'errorMessage',
+    'dataPath': '/properties/0/type',
+    'schemaPath': '#/properties/properties/items/allOf/3/then/properties/type/errorMessage',
+    'params': {
+      'errors': [
+        {
+          'keyword': 'enum',
+          'dataPath': '/properties/0/type',
+          'schemaPath': '#/properties/properties/items/allOf/3/then/properties/type/enum',
+          'params': {
+            'allowedValues': [
+              'String',
+              'Hidden',
+              'Dropdown'
+            ]
+          },
+          'message': 'should be equal to one of the allowed values'
+        }
+      ]
+    },
+    'message': 'invalid property type "Text" for binding type "camunda:outputParameter"; must be any of { String, Hidden, Dropdown }'
+  },
+  {
+    'keyword': 'if',
+    'dataPath': '/properties/0',
+    'schemaPath': '#/properties/properties/items/allOf/3/if',
+    'params': {
+      'failingKeyword': 'then'
+    },
+    'message': 'should match "then" schema'
+  }
+];
