287ecc46aa34dd564ea310407820860b39183163
Niklas Kiefer <niklas.kiefer@camunda.com>
2022-03-21 16:03:44 +0100

    feat(zeebe): add `documentationRef`
    Related to https://github.com/bpmn-io/bpmn-js-properties-panel/issues/614

---
+++ packages/zeebe-element-templates-json-schema/test/fixtures/documentation-ref.js
@@ -0,0 +1,11 @@
+export const template = {
+  'name': 'Documentation Ref',
+  'id': 'com.camunda.example.DocumentationRef',
+  'appliesTo': [
+    'bpmn:Task'
+  ],
+  'properties': [],
+  'documentationRef': 'https://www.example.com/myTask'
+};
+
+export const errors = null;
