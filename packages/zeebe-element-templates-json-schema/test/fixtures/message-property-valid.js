2b0bf369f669904c347cd21a37f5bef8dd4f7cb0
Maciej Barelkowski <maciej.barelkowski@camunda.com>
2023-11-07 18:22:07 +0100

    feat: support receive and send task templating
    Related to https://github.com/camunda/camunda-modeler/issues/3573

--- packages/zeebe-element-templates-json-schema/test/fixtures/message-property-valid.js
+++ packages/zeebe-element-templates-json-schema/test/fixtures/message-property-valid.js
@@ -1,23 +1,45 @@
-export const template = {
-  'name': 'MissingBindingName',
-  'id': 'com.camunda.example.MissingBindingName',
-  'appliesTo': [
-    'bpmn:IntermediateCatchEvent'
-  ],
-  'elementType': {
-    value: 'bpmn:IntermediateCatchEvent',
-    eventDefinition: 'bpmn:MessageEventDefinition'
-  },
-  'properties': [
-    {
-      'label': 'foo',
-      'type': 'String',
-      'binding': {
-        'type': 'bpmn:Message#property',
-        'name': 'name'
+export const template = [
+  {
+    'name': 'ValidTemplateEvent',
+    'id': 'com.camunda.example.ValidTemplateEvent',
+    'appliesTo': [
+      'bpmn:IntermediateCatchEvent'
+    ],
+    'elementType': {
+      value: 'bpmn:IntermediateCatchEvent',
+      eventDefinition: 'bpmn:MessageEventDefinition'
+    },
+    'properties': [
+      {
+        'label': 'foo',
+        'type': 'String',
+        'binding': {
+          'type': 'bpmn:Message#property',
+          'name': 'name'
+        }
       }
-    }
-  ]
-};
+    ]
+  },
+  {
+    'name': 'ValidTemplateReceiveTask',
+    'id': 'com.camunda.example.ValidTemplateReceiveTask',
+    'appliesTo': [
+      'bpmn:Task'
+    ],
+    'elementType': {
+      value: 'bpmn:ReceiveTask'
+    },
+    'properties': [
+      {
+        'label': 'foo',
+        'type': 'String',
+        'binding': {
+          'type': 'bpmn:Message#property',
+          'name': 'name'
+        }
+      }
+    ]
+  }
+];
 
 export const errors = null;
