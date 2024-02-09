84cf79a83010b4ba82a53863bfe1c032d0edcda3
Martin Stamm <martin.stamm@camunda.com>
2024-02-09 13:06:32 +0100

    chore: add failing `choices` tests

--- packages/zeebe-element-templates-json-schema/test/fixtures/condition-on-itself-dropdown-choices.js
+++ packages/zeebe-element-templates-json-schema/test/fixtures/condition-on-itself-dropdown-choices.js
@@ -31,6 +31,65 @@ export const template = {
         'type': 'zeebe:property',
         'name': 'method'
       }
+    },
+    {
+      'id': 'valid',
+      'label': 'Dropdown',
+      'type': 'Dropdown',
+      'choices': [
+        {
+          'value': 'opt1',
+          'name':'opt1'
+        },
+        {
+          'value': 'opt2',
+          'name': 'opt2'
+        },
+        {
+          'value': 'opt3',
+          'name': 'opt3',
+          'condition': {
+            'property': 'myId',
+            'equals': 'text'
+          }
+        }
+      ],
+      'binding': {
+        'type': 'zeebe:property',
+        'name': 'method'
+      }
+    },
+    {
+      'id': 'validAllMatch',
+      'label': 'Dropdown',
+      'type': 'Dropdown',
+      'choices': [
+        {
+          'value': 'opt1',
+          'name':'opt1'
+        },
+        {
+          'value': 'opt2',
+          'name': 'opt2'
+        },
+        {
+          'value': 'opt3',
+          'name': 'opt3',
+          'condition': {
+            'allMatch': [
+              {
+                'property': 'myId',
+                'equals': 'text'
+              }
+            ]
+
+          }
+        }
+      ],
+      'binding': {
+        'type': 'zeebe:property',
+        'name': 'method'
+      }
     }
   ]
 };
