5ec0f3b07d71673dd4d0c47879704dc3fcf1b035
marcello.barile <marcello.barile@camunda.com>
2025-05-29 15:08:25 +0200

    fix: remove if block

--- packages/zeebe-element-templates-json-schema/test/fixtures/invalid-script-task-job-worker-and-script.js
+++ packages/zeebe-element-templates-json-schema/test/fixtures/invalid-script-task-job-worker-and-script.js
@@ -45,14 +45,14 @@ export const errors = [
   {
     'keyword': 'not',
     'dataPath': '',
-    'schemaPath': '#/allOf/1/allOf/6/then/not',
+    'schemaPath': '#/allOf/1/allOf/5/then/not',
     'params': {},
     'message': 'should NOT be valid'
   },
   {
     'keyword': 'if',
     'dataPath': '',
-    'schemaPath': '#/allOf/1/allOf/6/if',
+    'schemaPath': '#/allOf/1/allOf/5/if',
     'params': {
       'failingKeyword': 'then'
     },
