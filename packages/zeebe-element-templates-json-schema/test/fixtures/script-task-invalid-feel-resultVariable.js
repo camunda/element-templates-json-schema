89de89ab2c3b45709207e4b080c75d44f48ef700
marcello.barile <marcello.barile@camunda.com>
2025-06-10 12:02:07 +0200

    resultVariable must not be a feel expression

---
+++ packages/zeebe-element-templates-json-schema/test/fixtures/script-task-invalid-feel-resultVariable.js
@@ -0,0 +1,83 @@
+export const template = {
+  '$schema': 'https://unpkg.com/@camunda/zeebe-element-templates-json-schema/resources/schema.json',
+  'name': 'Reusable Rule Template',
+  'id': 'sometemplate',
+  'description': 'some description',
+  'version': 1,
+  'engines': {
+    'camunda': '^8.6'
+  },
+  'appliesTo': [
+    'bpmn:Task'
+  ],
+  'elementType': {
+    'value': 'bpmn:ScriptTask'
+  },
+  'properties': [
+    {
+      'type': 'String',
+      'value': '={"currentWeek": week of year(today()), "people": participants, "index": modulo(currentWeek, count(people)), "selectedPerson": people[index + 1]}.selectedPerson',
+      'binding': {
+        'type': 'zeebe:script',
+        'property': 'expression'
+      }
+    },
+    {
+      'type': 'String',
+      'feel': 'optional',
+      'value': 'aResultVariable',
+      'binding': {
+        'type': 'zeebe:script',
+        'property': 'resultVariable'
+      }
+    }
+  ]
+};
+
+export const errors = [
+  {
+    'keyword': 'errorMessage',
+    'dataPath': '/properties/1',
+    'schemaPath': '#/allOf/1/items/allOf/13/then/errorMessage',
+    'params': {
+      'errors': [
+        {
+          'keyword': 'not',
+          'dataPath': '/properties/1',
+          'schemaPath': '#/allOf/1/items/allOf/13/then/not',
+          'params': {},
+          'message': 'should NOT be valid',
+          'emUsed': true
+        }
+      ]
+    },
+    'message': 'Property "resultVariable" cannot be a Feel expression'
+  },
+  {
+    'keyword': 'if',
+    'dataPath': '/properties/1',
+    'schemaPath': '#/allOf/1/items/allOf/13/if',
+    'params': {
+      'failingKeyword': 'then'
+    },
+    'message': 'should match "then" schema'
+  },
+  {
+    'keyword': 'type',
+    'dataPath': '',
+    'schemaPath': '#/oneOf/1/type',
+    'params': {
+      'type': 'array'
+    },
+    'message': 'should be array'
+  },
+  {
+    'keyword': 'oneOf',
+    'dataPath': '',
+    'schemaPath': '#/oneOf',
+    'params': {
+      'passingSchemas': null
+    },
+    'message': 'should match exactly one schema in oneOf'
+  }
+];
