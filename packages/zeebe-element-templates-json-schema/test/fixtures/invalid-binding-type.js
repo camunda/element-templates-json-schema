d7fd1762e2ef0f8378f289cf4998f1c9877ff1d2
Anton von Weltzien <anton.weltzien@camunda.com>
2025-06-17 14:24:06 +0200

    chore: add form:Definition to possible binding types error message

--- packages/zeebe-element-templates-json-schema/test/fixtures/invalid-binding-type.js
+++ packages/zeebe-element-templates-json-schema/test/fixtures/invalid-binding-type.js
@@ -58,7 +58,7 @@ export const errors = [
         }
       ]
     },
-    message: 'invalid property.binding type "foo"; must be any of { property, zeebe:taskDefinition:type, zeebe:input, zeebe:output, zeebe:property, zeebe:taskHeader, bpmn:Message#property, bpmn:Message#zeebe:subscription#property, zeebe:taskDefinition, zeebe:calledElement, zeebe:linkedResource, zeebe:userTask, zeebe:calledDecision }'
+    message: 'invalid property.binding type "foo"; must be any of { property, zeebe:taskDefinition:type, zeebe:input, zeebe:output, zeebe:property, zeebe:taskHeader, bpmn:Message#property, bpmn:Message#zeebe:subscription#property, zeebe:taskDefinition, zeebe:calledElement, zeebe:linkedResource, zeebe:userTask, zeebe:formDefinition, zeebe:calledDecision }'
   },
   {
     dataPath: '',
