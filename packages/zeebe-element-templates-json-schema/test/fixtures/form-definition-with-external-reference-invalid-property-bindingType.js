0b7117c47006cbae494d4a99db47912c858d321f
Anton von Weltzien <anton.weltzien@camunda.com>
2025-06-24 16:02:46 +0200

    feat: `bindingType` can only exist for formId style user tasks

--- packages/zeebe-element-templates-json-schema/test/fixtures/form-definition-with-external-reference-invalid-property-bindingType.js
+++ packages/zeebe-element-templates-json-schema/test/fixtures/form-definition-with-external-reference-invalid-property-bindingType.js
@@ -34,4 +34,77 @@ export const template = {
   ]
 };
 
-export const errors = [ {} ];
+export const errors = [
+  {
+    keyword: 'required',
+    dataPath: '/properties/0/binding',
+    schemaPath: '#/allOf/1/allOf/8/then/properties/properties/contains/properties/binding/required',
+    params: {
+      missingProperty: 'property'
+    },
+    message: "should have required property 'property'"
+  },
+  {
+    keyword: 'const',
+    dataPath: '/properties/0/binding/type',
+    schemaPath: '#/allOf/1/allOf/8/then/properties/properties/contains/properties/binding/properties/type/const',
+    params: {
+      allowedValue: 'zeebe:formDefinition'
+    },
+    message: 'should be equal to constant'
+  },
+  {
+    keyword: 'const',
+    dataPath: '/properties/1/binding/property',
+    schemaPath: '#/allOf/1/allOf/8/then/properties/properties/contains/properties/binding/properties/property/const',
+    params: {
+      allowedValue: 'formId'
+    },
+    message: 'should be equal to constant'
+  },
+  {
+    keyword: 'const',
+    dataPath: '/properties/2/binding/property',
+    schemaPath: '#/allOf/1/allOf/8/then/properties/properties/contains/properties/binding/properties/property/const',
+    params: {
+      allowedValue: 'formId'
+    },
+    message: 'should be equal to constant'
+  },
+  {
+    keyword: 'contains',
+    dataPath: '/properties',
+    schemaPath: '#/allOf/1/allOf/8/then/properties/properties/contains',
+    params: {
+      minContains: 1
+    },
+    message: 'should contain at least 1 valid item(s)'
+  },
+  {
+    keyword: 'if',
+    dataPath: '',
+    schemaPath: '#/allOf/1/allOf/8/if',
+    params: {
+      failingKeyword: 'then'
+    },
+    message: 'should match "then" schema'
+  },
+  {
+    keyword: 'type',
+    dataPath: '',
+    schemaPath: '#/oneOf/1/type',
+    params: {
+      type: 'array'
+    },
+    message: 'should be array'
+  },
+  {
+    keyword: 'oneOf',
+    dataPath: '',
+    schemaPath: '#/oneOf',
+    params: {
+      passingSchemas: null
+    },
+    message: 'should match exactly one schema in oneOf'
+  }
+];
