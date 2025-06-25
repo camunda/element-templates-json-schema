992cb8c81e008089b9387a3b7359aa669581a0b3
Anton von Weltzien <anton.weltzien@camunda.com>
2025-06-25 13:43:50 +0200

    test: add additional coverage for incorrect input type tests

--- packages/zeebe-element-templates-json-schema/test/fixtures/properties/binding-type-invalid-input-type.js
+++ packages/zeebe-element-templates-json-schema/test/fixtures/properties/binding-type-invalid-input-type.js
@@ -7,6 +7,30 @@ export const properties = [
       'type': 'to be inserted by `itWithProperties`',
       'property': 'bindingType'
     }
+  },
+  {
+    'type': 'Number',
+    'value': 1,
+    'binding': {
+      'type': 'to be inserted by `itWithProperties`',
+      'property': 'bindingType'
+    }
+  },
+  {
+    'type': 'Text',
+    'value': 'latest',
+    'binding': {
+      'type': 'to be inserted by `itWithProperties`',
+      'property': 'bindingType'
+    }
+  },
+  {
+    'type': 'Boolean',
+    'value': true,
+    'binding': {
+      'type': 'to be inserted by `itWithProperties`',
+      'property': 'bindingType'
+    }
   }
 ];
 
@@ -38,6 +62,128 @@ export const errors = [
   {
     keyword: 'if',
     dataPath: '/properties/2',
+    schemaPath: '#/allOf/1/items/allOf/17/if',
+    params: {
+      failingKeyword: 'then'
+    },
+    message: 'should match "then" schema'
+  },
+  {
+    keyword: 'errorMessage',
+    dataPath: '/properties/3/type',
+    schemaPath: '#/allOf/1/items/allOf/17/then/properties/type/errorMessage',
+    params: {
+      errors: [
+        {
+          keyword: 'enum',
+          dataPath: '/properties/3/type',
+          schemaPath: '#/allOf/1/items/allOf/17/then/properties/type/enum',
+          params: {
+            allowedValues: [
+              'Hidden',
+              'Dropdown'
+            ]
+          },
+          message: 'should be equal to one of the allowed values',
+          emUsed: true
+        }
+      ]
+    },
+    message: 'Incorrect type "Number". Must be one of { Hidden, Dropdown } for binding with `property`=`bindingType`'
+  },
+  {
+    keyword: 'enum',
+    dataPath: '/properties/3/value',
+    schemaPath: '#/allOf/1/items/allOf/17/then/properties/value/enum',
+    params: {
+      allowedValues: [
+        'latest',
+        'versionTag',
+        'deployment'
+      ]
+    },
+    message: 'should be equal to one of the allowed values'
+  },
+  {
+    keyword: 'if',
+    dataPath: '/properties/3',
+    schemaPath: '#/allOf/1/items/allOf/17/if',
+    params: {
+      failingKeyword: 'then'
+    },
+    message: 'should match "then" schema'
+  },
+  {
+    keyword: 'errorMessage',
+    dataPath: '/properties/4/type',
+    schemaPath: '#/allOf/1/items/allOf/17/then/properties/type/errorMessage',
+    params: {
+      errors: [
+        {
+          keyword: 'enum',
+          dataPath: '/properties/4/type',
+          schemaPath: '#/allOf/1/items/allOf/17/then/properties/type/enum',
+          params: {
+            allowedValues: [
+              'Hidden',
+              'Dropdown'
+            ]
+          },
+          message: 'should be equal to one of the allowed values',
+          emUsed: true
+        }
+      ]
+    },
+    message: 'Incorrect type "Text". Must be one of { Hidden, Dropdown } for binding with `property`=`bindingType`'
+  },
+  {
+    keyword: 'if',
+    dataPath: '/properties/4',
+    schemaPath: '#/allOf/1/items/allOf/17/if',
+    params: {
+      failingKeyword: 'then'
+    },
+    message: 'should match "then" schema'
+  },
+  {
+    keyword: 'errorMessage',
+    dataPath: '/properties/5/type',
+    schemaPath: '#/allOf/1/items/allOf/17/then/properties/type/errorMessage',
+    params: {
+      errors: [
+        {
+          keyword: 'enum',
+          dataPath: '/properties/5/type',
+          schemaPath: '#/allOf/1/items/allOf/17/then/properties/type/enum',
+          params: {
+            allowedValues: [
+              'Hidden',
+              'Dropdown'
+            ]
+          },
+          message: 'should be equal to one of the allowed values',
+          emUsed: true
+        }
+      ]
+    },
+    message: 'Incorrect type "Boolean". Must be one of { Hidden, Dropdown } for binding with `property`=`bindingType`'
+  },
+  {
+    keyword: 'enum',
+    dataPath: '/properties/5/value',
+    schemaPath: '#/allOf/1/items/allOf/17/then/properties/value/enum',
+    params: {
+      allowedValues: [
+        'latest',
+        'versionTag',
+        'deployment'
+      ]
+    },
+    message: 'should be equal to one of the allowed values'
+  },
+  {
+    keyword: 'if',
+    dataPath: '/properties/5',
     schemaPath: '#/allOf/1/items/allOf/17/if',
     params: {
       failingKeyword: 'then'
--- packages/zeebe-element-templates-json-schema/test/fixtures/properties/binding-type-versionTag-invalid-input-type.js
+++ packages/zeebe-element-templates-json-schema/test/fixtures/properties/binding-type-versionTag-invalid-input-type.js
@@ -15,6 +15,14 @@ export const properties = [
       'type': 'to be inserted by `itWithProperties`',
       'property': 'versionTag'
     }
+  },
+  {
+    'type': 'Boolean',
+    'value': true,
+    'binding': {
+      'type': 'to be inserted by `itWithProperties`',
+      'property': 'versionTag'
+    }
   }
 ];
 
@@ -55,6 +63,40 @@ export const errors = [
     message: 'should match "then" schema'
   },
   {
+    keyword: 'errorMessage',
+    dataPath: '/properties/4/type',
+    schemaPath: '#/allOf/1/items/allOf/18/then/allOf/0/properties/type/errorMessage',
+    params: {
+      errors: [
+        {
+          keyword: 'enum',
+          dataPath: '/properties/4/type',
+          schemaPath: '#/allOf/1/items/allOf/18/then/allOf/0/properties/type/enum',
+          params: {
+            allowedValues: [
+              'Hidden',
+              'Dropdown',
+              'String',
+              'Text'
+            ]
+          },
+          message: 'should be equal to one of the allowed values',
+          emUsed: true
+        }
+      ]
+    },
+    message: 'Incorrect type "Boolean". Must be one of { String, Text, Hidden, Dropdown } for binding with `property`=`versionTag`'
+  },
+  {
+    keyword: 'if',
+    dataPath: '/properties/4',
+    schemaPath: '#/allOf/1/items/allOf/18/if',
+    params: {
+      failingKeyword: 'then'
+    },
+    message: 'should match "then" schema'
+  },
+  {
     keyword: 'type',
     dataPath: '',
     schemaPath: '#/oneOf/1/type',
@@ -72,4 +114,5 @@ export const errors = [
     },
     message: 'should match exactly one schema in oneOf'
   }
-];
+]
+;
