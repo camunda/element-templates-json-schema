39a15b8ca443a453a8a2caa545a864e1e58204c5
Anton von Weltzien <anton.weltzien@camunda.com>
2025-06-27 12:03:55 +0200

    test: update test for invalid property on assignment

--- packages/zeebe-element-templates-json-schema/test/fixtures/assignment-definition-invalid-property.js
+++ packages/zeebe-element-templates-json-schema/test/fixtures/assignment-definition-invalid-property.js
@@ -26,4 +26,45 @@ export const template = {
   ]
 };
 
-export const errors = [ {} ];
+export const errors = [
+  {
+    keyword: 'enum',
+    dataPath: '/properties/1/binding/property',
+    schemaPath: '#/allOf/1/items/properties/binding/allOf/9/then/properties/property/enum',
+    params: {
+      allowedValues: [
+        'assignee',
+        'candidateUsers',
+        'candidateGroups'
+      ]
+    },
+    message: 'should be equal to one of the allowed values'
+  },
+  {
+    keyword: 'if',
+    dataPath: '/properties/1/binding',
+    schemaPath: '#/allOf/1/items/properties/binding/allOf/9/if',
+    params: {
+      failingKeyword: 'then'
+    },
+    message: 'should match "then" schema'
+  },
+  {
+    keyword: 'type',
+    dataPath: '',
+    schemaPath: '#/oneOf/1/type',
+    params: {
+      type: 'array'
+    },
+    message: 'should be array'
+  },
+  {
+    keyword: 'oneOf',
+    dataPath: '',
+    schemaPath: '#/oneOf',
+    params: {
+      passingSchemas: null
+    },
+    message: 'should match exactly one schema in oneOf'
+  }
+];
