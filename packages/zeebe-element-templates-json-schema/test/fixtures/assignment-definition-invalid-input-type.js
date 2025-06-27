f1a6f904340fbf403924b770f983e68696444922
Anton von Weltzien <anton.weltzien@camunda.com>
2025-06-27 12:02:55 +0200

    feat: enforce stringly input type for any assignment property

--- packages/zeebe-element-templates-json-schema/test/fixtures/assignment-definition-invalid-input-type.js
+++ packages/zeebe-element-templates-json-schema/test/fixtures/assignment-definition-invalid-input-type.js
@@ -63,4 +63,161 @@ export const template = {
   ]
 };
 
-export const errors = [ {} ];
+export const errors = [
+  {
+    keyword: 'enum',
+    dataPath: '/properties/1/type',
+    schemaPath: '#/allOf/1/items/allOf/22/then/properties/type/enum',
+    params: {
+      allowedValues: [
+        'Hidden',
+        'String',
+        'Text',
+        'Dropdown'
+      ]
+    },
+    message: 'should be equal to one of the allowed values'
+  },
+  {
+    keyword: 'if',
+    dataPath: '/properties/1',
+    schemaPath: '#/allOf/1/items/allOf/22/if',
+    params: {
+      failingKeyword: 'then'
+    },
+    message: 'should match "then" schema'
+  },
+  {
+    keyword: 'enum',
+    dataPath: '/properties/2/type',
+    schemaPath: '#/allOf/1/items/allOf/22/then/properties/type/enum',
+    params: {
+      allowedValues: [
+        'Hidden',
+        'String',
+        'Text',
+        'Dropdown'
+      ]
+    },
+    message: 'should be equal to one of the allowed values'
+  },
+  {
+    keyword: 'if',
+    dataPath: '/properties/2',
+    schemaPath: '#/allOf/1/items/allOf/22/if',
+    params: {
+      failingKeyword: 'then'
+    },
+    message: 'should match "then" schema'
+  },
+  {
+    keyword: 'enum',
+    dataPath: '/properties/3/type',
+    schemaPath: '#/allOf/1/items/allOf/22/then/properties/type/enum',
+    params: {
+      allowedValues: [
+        'Hidden',
+        'String',
+        'Text',
+        'Dropdown'
+      ]
+    },
+    message: 'should be equal to one of the allowed values'
+  },
+  {
+    keyword: 'if',
+    dataPath: '/properties/3',
+    schemaPath: '#/allOf/1/items/allOf/22/if',
+    params: {
+      failingKeyword: 'then'
+    },
+    message: 'should match "then" schema'
+  },
+  {
+    keyword: 'enum',
+    dataPath: '/properties/4/type',
+    schemaPath: '#/allOf/1/items/allOf/22/then/properties/type/enum',
+    params: {
+      allowedValues: [
+        'Hidden',
+        'String',
+        'Text',
+        'Dropdown'
+      ]
+    },
+    message: 'should be equal to one of the allowed values'
+  },
+  {
+    keyword: 'if',
+    dataPath: '/properties/4',
+    schemaPath: '#/allOf/1/items/allOf/22/if',
+    params: {
+      failingKeyword: 'then'
+    },
+    message: 'should match "then" schema'
+  },
+  {
+    keyword: 'enum',
+    dataPath: '/properties/5/type',
+    schemaPath: '#/allOf/1/items/allOf/22/then/properties/type/enum',
+    params: {
+      allowedValues: [
+        'Hidden',
+        'String',
+        'Text',
+        'Dropdown'
+      ]
+    },
+    message: 'should be equal to one of the allowed values'
+  },
+  {
+    keyword: 'if',
+    dataPath: '/properties/5',
+    schemaPath: '#/allOf/1/items/allOf/22/if',
+    params: {
+      failingKeyword: 'then'
+    },
+    message: 'should match "then" schema'
+  },
+  {
+    keyword: 'enum',
+    dataPath: '/properties/6/type',
+    schemaPath: '#/allOf/1/items/allOf/22/then/properties/type/enum',
+    params: {
+      allowedValues: [
+        'Hidden',
+        'String',
+        'Text',
+        'Dropdown'
+      ]
+    },
+    message: 'should be equal to one of the allowed values'
+  },
+  {
+    keyword: 'if',
+    dataPath: '/properties/6',
+    schemaPath: '#/allOf/1/items/allOf/22/if',
+    params: {
+      failingKeyword: 'then'
+    },
+    message: 'should match "then" schema'
+  },
+  {
+    keyword: 'type',
+    dataPath: '',
+    schemaPath: '#/oneOf/1/type',
+    params: {
+      type: 'array'
+    },
+    message: 'should be array'
+  },
+  {
+    keyword: 'oneOf',
+    dataPath: '',
+    schemaPath: '#/oneOf',
+    params: {
+      passingSchemas: null
+    },
+    message: 'should match exactly one schema in oneOf'
+  }
+];
