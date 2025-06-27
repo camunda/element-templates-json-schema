fa9986f8950f07e655eaf2559be4341822b8b7c2
Anton von Weltzien <anton.weltzien@camunda.com>
2025-06-27 16:36:15 +0200

    feat: basic support

--- packages/zeebe-element-templates-json-schema/test/fixtures/priority-definition-invalid-input-type.js
+++ packages/zeebe-element-templates-json-schema/test/fixtures/priority-definition-invalid-input-type.js
@@ -7,7 +7,7 @@ export const template = {
     'bpmn:Task'
   ],
   'elementType': {
-    'value': 'bpmn:Task'
+    'value': 'bpmn:UserTask'
   },
   'properties': [
     {
@@ -56,4 +56,188 @@ export const template = {
   ]
 };
 
-export const errors = [ {} ];
+export const errors = [
+  {
+    keyword: 'enum',
+    dataPath: '/properties/1/type',
+    schemaPath: '#/allOf/1/items/allOf/23/then/anyOf/0/properties/type/enum',
+    params: {
+      allowedValues: [
+        'Hidden',
+        'Number',
+        'Dropdown'
+      ]
+    },
+    message: 'should be equal to one of the allowed values'
+  },
+  {
+    keyword: 'required',
+    dataPath: '/properties/1',
+    schemaPath: '#/allOf/1/items/allOf/23/then/anyOf/1/required',
+    params: {
+      missingProperty: 'feel'
+    },
+    message: "should have required property 'feel'"
+  },
+  {
+    keyword: 'anyOf',
+    dataPath: '/properties/1',
+    schemaPath: '#/allOf/1/items/allOf/23/then/anyOf',
+    params: {},
+    message: 'should match some schema in anyOf'
+  },
+  {
+    keyword: 'if',
+    dataPath: '/properties/1',
+    schemaPath: '#/allOf/1/items/allOf/23/if',
+    params: {
+      failingKeyword: 'then'
+    },
+    message: 'should match "then" schema'
+  },
+  {
+    keyword: 'enum',
+    dataPath: '/properties/2/type',
+    schemaPath: '#/allOf/1/items/allOf/23/then/anyOf/0/properties/type/enum',
+    params: {
+      allowedValues: [
+        'Hidden',
+        'Number',
+        'Dropdown'
+      ]
+    },
+    message: 'should be equal to one of the allowed values'
+  },
+  {
+    keyword: 'const',
+    dataPath: '/properties/2/feel',
+    schemaPath: '#/allOf/1/items/allOf/23/then/anyOf/1/properties/feel/const',
+    params: {
+      allowedValue: 'required'
+    },
+    message: 'should be equal to constant'
+  },
+  {
+    keyword: 'anyOf',
+    dataPath: '/properties/2',
+    schemaPath: '#/allOf/1/items/allOf/23/then/anyOf',
+    params: {},
+    message: 'should match some schema in anyOf'
+  },
+  {
+    keyword: 'if',
+    dataPath: '/properties/2',
+    schemaPath: '#/allOf/1/items/allOf/23/if',
+    params: {
+      failingKeyword: 'then'
+    },
+    message: 'should match "then" schema'
+  },
+  {
+    keyword: 'enum',
+    dataPath: '/properties/3/type',
+    schemaPath: '#/allOf/1/items/allOf/23/then/anyOf/0/properties/type/enum',
+    params: {
+      allowedValues: [
+        'Hidden',
+        'Number',
+        'Dropdown'
+      ]
+    },
+    message: 'should be equal to one of the allowed values'
+  },
+  {
+    keyword: 'required',
+    dataPath: '/properties/3',
+    schemaPath: '#/allOf/1/items/allOf/23/then/anyOf/1/required',
+    params: {
+      missingProperty: 'feel'
+    },
+    message: "should have required property 'feel'"
+  },
+  {
+    keyword: 'enum',
+    dataPath: '/properties/3/type',
+    schemaPath: '#/allOf/1/items/allOf/23/then/anyOf/1/properties/type/enum',
+    params: {
+      allowedValues: [
+        'String',
+        'Text'
+      ]
+    },
+    message: 'should be equal to one of the allowed values'
+  },
+  {
+    keyword: 'anyOf',
+    dataPath: '/properties/3',
+    schemaPath: '#/allOf/1/items/allOf/23/then/anyOf',
+    params: {},
+    message: 'should match some schema in anyOf'
+  },
+  {
+    keyword: 'if',
+    dataPath: '/properties/3',
+    schemaPath: '#/allOf/1/items/allOf/23/if',
+    params: {
+      failingKeyword: 'then'
+    },
+    message: 'should match "then" schema'
+  },
+  {
+    keyword: 'enum',
+    dataPath: '/properties/4/type',
+    schemaPath: '#/allOf/1/items/allOf/23/then/anyOf/0/properties/type/enum',
+    params: {
+      allowedValues: [
+        'Hidden',
+        'Number',
+        'Dropdown'
+      ]
+    },
+    message: 'should be equal to one of the allowed values'
+  },
+  {
+    keyword: 'required',
+    dataPath: '/properties/4',
+    schemaPath: '#/allOf/1/items/allOf/23/then/anyOf/1/required',
+    params: {
+      missingProperty: 'feel'
+    },
+    message: "should have required property 'feel'"
+  },
+  {
+    keyword: 'anyOf',
+    dataPath: '/properties/4',
+    schemaPath: '#/allOf/1/items/allOf/23/then/anyOf',
+    params: {},
+    message: 'should match some schema in anyOf'
+  },
+  {
+    keyword: 'if',
+    dataPath: '/properties/4',
+    schemaPath: '#/allOf/1/items/allOf/23/if',
+    params: {
+      failingKeyword: 'then'
+    },
+    message: 'should match "then" schema'
+  },
+  {
+    keyword: 'type',
+    dataPath: '',
+    schemaPath: '#/oneOf/1/type',
+    params: {
+      type: 'array'
+    },
+    message: 'should be array'
+  },
+  {
+    keyword: 'oneOf',
+    dataPath: '',
+    schemaPath: '#/oneOf',
+    params: {
+      passingSchemas: null
+    },
+    message: 'should match exactly one schema in oneOf'
+  }
+]
+;
