aa7e289f49d1bef50205a37c60a79ced1c65ba9f
Anton von Weltzien <anton.weltzien@camunda.com>
2025-07-01 11:37:57 +0200

    feat: enforce that value for priority must be [0,100]

--- packages/zeebe-element-templates-json-schema/test/fixtures/priority-definition-invalid-value.js
+++ packages/zeebe-element-templates-json-schema/test/fixtures/priority-definition-invalid-value.js
@@ -20,7 +20,7 @@ export const template = {
       label: 'Prio',
       value: -1,
       description: 'Prio for task',
-      type: 'Integer',
+      type: 'Number',
       binding: {
         type: 'zeebe:priorityDefinition',
         property: 'priority'
@@ -30,13 +30,271 @@ export const template = {
       label: 'Prio',
       value: 101,
       description: 'Prio for task',
-      type: 'Integer',
+      type: 'Number',
       binding: {
         type: 'zeebe:priorityDefinition',
         property: 'priority'
       }
+    },
+    {
+      value: '101',
+      type: 'Hidden',
+      binding: {
+        type: 'zeebe:priorityDefinition',
+        property: 'priority'
+      }
+    },
+    {
+      value: '-1',
+      type: 'Hidden',
+      binding: {
+        type: 'zeebe:priorityDefinition',
+        property: 'priority'
+      }
+    },
+    {
+      'label': 'Prio',
+      'value': '101',
+      'description': 'Prio for task',
+      'constraints': {
+        'notEmpty': true
+      },
+      'type': 'Dropdown',
+      'binding': {
+        'type': 'zeebe:priorityDefinition',
+        'property': 'priority'
+      },
+      'choices': [
+        {
+          'name': 'Too High',
+          'value': '101'
+        },
+        {
+          'name': 'Not a number',
+          'value': 'abcd'
+        },
+        {
+          'name': 'Too Low',
+          'value': '-1'
+        }
+      ]
     }
+
+
   ]
 };
 
-export const errors = null;
+export const errors = [
+  {
+    keyword: 'minimum',
+    dataPath: '/properties/1/value',
+    schemaPath: '#/allOf/1/items/allOf/24/then/properties/value/minimum',
+    params: {
+      comparison: '>=',
+      limit: 0
+    },
+    message: 'should be >= 0'
+  },
+  {
+    keyword: 'if',
+    dataPath: '/properties/1',
+    schemaPath: '#/allOf/1/items/allOf/24/if',
+    params: {
+      failingKeyword: 'then'
+    },
+    message: 'should match "then" schema'
+  },
+  {
+    keyword: 'maximum',
+    dataPath: '/properties/2/value',
+    schemaPath: '#/allOf/1/items/allOf/24/then/properties/value/maximum',
+    params: {
+      comparison: '<=',
+      limit: 100
+    },
+    message: 'should be <= 100'
+  },
+  {
+    keyword: 'if',
+    dataPath: '/properties/2',
+    schemaPath: '#/allOf/1/items/allOf/24/if',
+    params: {
+      failingKeyword: 'then'
+    },
+    message: 'should match "then" schema'
+  },
+  {
+    keyword: 'errorMessage',
+    dataPath: '/properties/3/value',
+    schemaPath: '#/allOf/1/items/allOf/25/then/properties/value/errorMessage',
+    params: {
+      errors: [
+        {
+          keyword: 'pattern',
+          dataPath: '/properties/3/value',
+          schemaPath: '#/allOf/1/items/allOf/25/then/properties/value/pattern',
+          params: {
+            pattern: '^(100|[1-9]?[0-9])$'
+          },
+          message: 'should match pattern "^(100|[1-9]?[0-9])$"',
+          emUsed: true
+        }
+      ]
+    },
+    message: 'Invalid value for priority. Must be between 0 and 100.'
+  },
+  {
+    keyword: 'if',
+    dataPath: '/properties/3',
+    schemaPath: '#/allOf/1/items/allOf/25/if',
+    params: {
+      failingKeyword: 'then'
+    },
+    message: 'should match "then" schema'
+  },
+  {
+    keyword: 'errorMessage',
+    dataPath: '/properties/4/value',
+    schemaPath: '#/allOf/1/items/allOf/25/then/properties/value/errorMessage',
+    params: {
+      errors: [
+        {
+          keyword: 'pattern',
+          dataPath: '/properties/4/value',
+          schemaPath: '#/allOf/1/items/allOf/25/then/properties/value/pattern',
+          params: {
+            pattern: '^(100|[1-9]?[0-9])$'
+          },
+          message: 'should match pattern "^(100|[1-9]?[0-9])$"',
+          emUsed: true
+        }
+      ]
+    },
+    message: 'Invalid value for priority. Must be between 0 and 100.'
+  },
+  {
+    keyword: 'if',
+    dataPath: '/properties/4',
+    schemaPath: '#/allOf/1/items/allOf/25/if',
+    params: {
+      failingKeyword: 'then'
+    },
+    message: 'should match "then" schema'
+  },
+  {
+    keyword: 'errorMessage',
+    dataPath: '/properties/5/value',
+    schemaPath: '#/allOf/1/items/allOf/25/then/properties/value/errorMessage',
+    params: {
+      errors: [
+        {
+          keyword: 'pattern',
+          dataPath: '/properties/5/value',
+          schemaPath: '#/allOf/1/items/allOf/25/then/properties/value/pattern',
+          params: {
+            pattern: '^(100|[1-9]?[0-9])$'
+          },
+          message: 'should match pattern "^(100|[1-9]?[0-9])$"',
+          emUsed: true
+        }
+      ]
+    },
+    message: 'Invalid value for priority. Must be between 0 and 100.'
+  },
+  {
+    keyword: 'if',
+    dataPath: '/properties/5',
+    schemaPath: '#/allOf/1/items/allOf/25/if',
+    params: {
+      failingKeyword: 'then'
+    },
+    message: 'should match "then" schema'
+  },
+  {
+    keyword: 'errorMessage',
+    dataPath: '/properties/5/choices/0/value',
+    schemaPath: '#/allOf/1/items/allOf/26/then/properties/choices/items/allOf/1/properties/value/errorMessage',
+    params: {
+      errors: [
+        {
+          keyword: 'pattern',
+          dataPath: '/properties/5/choices/0/value',
+          schemaPath: '#/allOf/1/items/allOf/26/then/properties/choices/items/allOf/1/properties/value/pattern',
+          params: {
+            pattern: '^(100|[1-9]?[0-9])$'
+          },
+          message: 'should match pattern "^(100|[1-9]?[0-9])$"',
+          emUsed: true
+        }
+      ]
+    },
+    message: 'Invalid value for priority. Must be between 0 and 100.'
+  },
+  {
+    keyword: 'errorMessage',
+    dataPath: '/properties/5/choices/1/value',
+    schemaPath: '#/allOf/1/items/allOf/26/then/properties/choices/items/allOf/1/properties/value/errorMessage',
+    params: {
+      errors: [
+        {
+          keyword: 'pattern',
+          dataPath: '/properties/5/choices/1/value',
+          schemaPath: '#/allOf/1/items/allOf/26/then/properties/choices/items/allOf/1/properties/value/pattern',
+          params: {
+            pattern: '^(100|[1-9]?[0-9])$'
+          },
+          message: 'should match pattern "^(100|[1-9]?[0-9])$"',
+          emUsed: true
+        }
+      ]
+    },
+    message: 'Invalid value for priority. Must be between 0 and 100.'
+  },
+  {
+    keyword: 'errorMessage',
+    dataPath: '/properties/5/choices/2/value',
+    schemaPath: '#/allOf/1/items/allOf/26/then/properties/choices/items/allOf/1/properties/value/errorMessage',
+    params: {
+      errors: [
+        {
+          keyword: 'pattern',
+          dataPath: '/properties/5/choices/2/value',
+          schemaPath: '#/allOf/1/items/allOf/26/then/properties/choices/items/allOf/1/properties/value/pattern',
+          params: {
+            pattern: '^(100|[1-9]?[0-9])$'
+          },
+          message: 'should match pattern "^(100|[1-9]?[0-9])$"',
+          emUsed: true
+        }
+      ]
+    },
+    message: 'Invalid value for priority. Must be between 0 and 100.'
+  },
+  {
+    keyword: 'if',
+    dataPath: '/properties/5',
+    schemaPath: '#/allOf/1/items/allOf/26/if',
+    params: {
+      failingKeyword: 'then'
+    },
+    message: 'should match "then" schema'
+  },
+  {
+    keyword: 'type',
+    dataPath: '',
+    schemaPath: '#/oneOf/1/type',
+    params: {
+      type: 'array'
+    },
+    message: 'should be array'
+  },
+  {
+    keyword: 'oneOf',
+    dataPath: '',
+    schemaPath: '#/oneOf',
+    params: {
+      passingSchemas: null
+    },
+    message: 'should match exactly one schema in oneOf'
+  }
+];
