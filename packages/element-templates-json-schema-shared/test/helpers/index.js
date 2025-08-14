c772a0b75069f293e95a38168be629af72c2c6ae
Abdul Ahad <abdul.ahad@camunda.com>
2025-08-14 17:21:38 +0200

    chore: cleanup

--- packages/element-templates-json-schema-shared/test/helpers/index.js
+++ packages/element-templates-json-schema-shared/test/helpers/index.js
@@ -1,17 +1,10 @@
-const {
-  forEach,
-  set
-} = require('min-dash');
-
-const chai = require('chai');
-
 const { default: Ajv } = require('ajv');
 const AjvErrors = require('ajv-errors');
 
+const { withErrorMessages, withDeprecationWarnings } = require('./utils');
+
 module.exports = {
-  createValidator,
-  withErrorMessages,
-  withDeprecationWarnings
+  createValidator
 };
 
 function createValidator(schema, errors, deprecations) {
@@ -78,81 +71,4 @@ function createValidator(schema, errors, deprecations) {
   };
 
   return wrappedValidator;
-}
-
-function withErrorMessages(schema, errors) {
-
-  if (!errors || !errors.length) {
-    return schema;
-  }
-
-  // clone a new copy
-  let newSchema = JSON.parse(JSON.stringify(schema));
-
-  // set <errorMessage> keyword for given path
-  forEach(errors, function(error) {
-    newSchema = setErrorMessage(newSchema, error);
-  });
-
-  return newSchema;
-}
-
-function setErrorMessage(schema, error) {
-  const {
-    path,
-    errorMessage
-  } = error;
-
-  const errorMessagePath = [
-    ...path,
-    'errorMessage'
-  ];
-
-  return set(schema, errorMessagePath, errorMessage);
-}
-
-function eqlErrors(chai, utils) {
-
-  const Assertion = chai.Assertion;
-
-  Assertion.addMethod('eqlErrors', function(expectedErrors, filter) {
-
-    const actualErrors = this._obj;
-
-    // formats the validation errors, so that they can be used directly in the fixture files.
-    this.eql(expectedErrors,
-      `Errors from validation do not match expected.\n\tValidation returned this error (you can use it in the fixture):\n\t${JSON.stringify(actualErrors, null, 2).replace(/"([^"]+)":/g, '$1:')}\n`);
-  });
-}
-
-chai.use(eqlErrors);
-
-function withDeprecationWarnings(schema, deprecations) {
-  if (!deprecations || !deprecations.length) {
-    return schema;
-  }
-
-  // clone a new copy
-  let newSchema = JSON.parse(JSON.stringify(schema));
-
-  // set deprecation warnings for given paths
-  forEach(deprecations, function(deprecation) {
-    newSchema = setDeprecationWarning(newSchema, deprecation);
-  });
-
-  return newSchema;
-}
-
-function setDeprecationWarning(schema, deprecation) {
-  const {
-    path,
-    warningMessage
-  } = deprecation;
-
-  const deprecationPath = [
-    ...path,
-    'deprecatedWarning'
-  ];
-
-  return set(schema, deprecationPath, warningMessage);
 }
