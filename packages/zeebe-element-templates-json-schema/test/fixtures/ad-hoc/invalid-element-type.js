a9ea00be2b412a881e76bd7b5c979a66ed07b5c1
Anton von Weltzien <anton.weltzien@camunda.com>
2025-08-15 10:46:37 +0200

    chore: fix lint errors

--- packages/zeebe-element-templates-json-schema/test/fixtures/ad-hoc/invalid-element-type.js
+++ packages/zeebe-element-templates-json-schema/test/fixtures/ad-hoc/invalid-element-type.js
@@ -1,103 +1,103 @@
 export const template = {
-    '$schema': 'https://unpkg.com/@camunda/zeebe-element-templates-json-schema/resources/schema.json',
-    'name': 'AdHoc Template',
-    'id': 'io.camunda.examples.AdHoc.InvalidElementType',
-    'version': 1,
-    'appliesTo': [ 'bpmn:Activity' ],
-    'elementType': { 'value': 'bpmn:' },
-    'properties': [
-        {
-            'type': 'Hidden',
-            'binding': { 'type': 'zeebe:taskDefinition', 'property': 'type' },
-            'value': 'io.camunda.agenticai:aiagent-job-worker:1'
-        },
-        {
-            'type': 'Hidden',
-            'binding': { 'type': 'zeebe:adHoc', 'property': 'outputCollection' },
-            'value': 'toolCallResults'
-        },
-        {
-            'type': 'String',
-            'binding': { 'type': 'zeebe:adHoc', 'property': 'outputElement' },
-            'feel': 'required',
-            'value': '={ id: toolCall._meta.id, name: toolCall._meta.name, content: toolCallResult }'
-        }
-    ]
+  '$schema': 'https://unpkg.com/@camunda/zeebe-element-templates-json-schema/resources/schema.json',
+  'name': 'AdHoc Template',
+  'id': 'io.camunda.examples.AdHoc.InvalidElementType',
+  'version': 1,
+  'appliesTo': [ 'bpmn:Activity' ],
+  'elementType': { 'value': 'bpmn:' },
+  'properties': [
+    {
+      'type': 'Hidden',
+      'binding': { 'type': 'zeebe:taskDefinition', 'property': 'type' },
+      'value': 'io.camunda.agenticai:aiagent-job-worker:1'
+    },
+    {
+      'type': 'Hidden',
+      'binding': { 'type': 'zeebe:adHoc', 'property': 'outputCollection' },
+      'value': 'toolCallResults'
+    },
+    {
+      'type': 'String',
+      'binding': { 'type': 'zeebe:adHoc', 'property': 'outputElement' },
+      'feel': 'required',
+      'value': '={ id: toolCall._meta.id, name: toolCall._meta.name, content: toolCallResult }'
+    }
+  ]
 };
 
 export const errors = [
-    {
-        keyword: "errorMessage",
-        dataPath: "/elementType/value",
-        schemaPath: "#/allOf/0/properties/elementType/properties/value/errorMessage",
-        params: {
-            errors: [
-                {
-                    keyword: "pattern",
-                    dataPath: "/elementType/value",
-                    schemaPath: "#/allOf/0/properties/elementType/properties/value/pattern",
-                    params: {
-                        pattern: "^[\\w\\d]+:[\\w\\d]+$"
-                    },
-                    message: "should match pattern \"^[\\w\\d]+:[\\w\\d]+$\"",
-                    emUsed: true
-                }
-            ]
-        },
-        message: "invalid item for \"elementType\", should contain namespaced property, example: \"bpmn:Task\""
+  {
+    keyword: 'errorMessage',
+    dataPath: '/elementType/value',
+    schemaPath: '#/allOf/0/properties/elementType/properties/value/errorMessage',
+    params: {
+      errors: [
+        {
+          keyword: 'pattern',
+          dataPath: '/elementType/value',
+          schemaPath: '#/allOf/0/properties/elementType/properties/value/pattern',
+          params: {
+            pattern: '^[\\w\\d]+:[\\w\\d]+$'
+          },
+          message: 'should match pattern "^[\\w\\d]+:[\\w\\d]+$"',
+          emUsed: true
+        }
+      ]
     },
-    {
-        keyword: "const",
-        dataPath: "/elementType/value",
-        schemaPath: "#/allOf/1/allOf/9/then/anyOf/0/properties/elementType/properties/value/const",
-        params: {
-            allowedValue: "bpmn:AdHocSubProcess"
-        },
-        message: "should be equal to constant"
+    message: 'invalid item for "elementType", should contain namespaced property, example: "bpmn:Task"'
+  },
+  {
+    keyword: 'const',
+    dataPath: '/elementType/value',
+    schemaPath: '#/allOf/1/allOf/9/then/anyOf/0/properties/elementType/properties/value/const',
+    params: {
+      allowedValue: 'bpmn:AdHocSubProcess'
     },
-    {
-        keyword: "const",
-        dataPath: "/appliesTo",
-        schemaPath: "#/allOf/1/allOf/9/then/anyOf/1/properties/appliesTo/const",
-        params: {
-            allowedValue: [
-                "bpmn:AdHocSubProcess"
-            ]
-        },
-        message: "should be equal to constant"
+    message: 'should be equal to constant'
+  },
+  {
+    keyword: 'const',
+    dataPath: '/appliesTo',
+    schemaPath: '#/allOf/1/allOf/9/then/anyOf/1/properties/appliesTo/const',
+    params: {
+      allowedValue: [
+        'bpmn:AdHocSubProcess'
+      ]
     },
-    {
-        keyword: "anyOf",
-        dataPath: "",
-        schemaPath: "#/allOf/1/allOf/9/then/anyOf",
-        params: {},
-        message: "should match some schema in anyOf"
+    message: 'should be equal to constant'
+  },
+  {
+    keyword: 'anyOf',
+    dataPath: '',
+    schemaPath: '#/allOf/1/allOf/9/then/anyOf',
+    params: {},
+    message: 'should match some schema in anyOf'
+  },
+  {
+    keyword: 'if',
+    dataPath: '',
+    schemaPath: '#/allOf/1/allOf/9/if',
+    params: {
+      failingKeyword: 'then'
     },
-    {
-        keyword: "if",
-        dataPath: "",
-        schemaPath: "#/allOf/1/allOf/9/if",
-        params: {
-            failingKeyword: "then"
-        },
-        message: "should match \"then\" schema"
+    message: 'should match "then" schema'
+  },
+  {
+    keyword: 'type',
+    dataPath: '',
+    schemaPath: '#/oneOf/1/type',
+    params: {
+      type: 'array'
     },
-    {
-        keyword: "type",
-        dataPath: "",
-        schemaPath: "#/oneOf/1/type",
-        params: {
-            type: "array"
-        },
-        message: "should be array"
+    message: 'should be array'
+  },
+  {
+    keyword: 'oneOf',
+    dataPath: '',
+    schemaPath: '#/oneOf',
+    params: {
+      passingSchemas: null
     },
-    {
-        keyword: "oneOf",
-        dataPath: "",
-        schemaPath: "#/oneOf",
-        params: {
-            passingSchemas: null
-        },
-        message: "should match exactly one schema in oneOf"
-    }
+    message: 'should match exactly one schema in oneOf'
+  }
 ];
--- packages/zeebe-element-templates-json-schema/test/fixtures/ad-hoc/invalid-property.js
+++ packages/zeebe-element-templates-json-schema/test/fixtures/ad-hoc/invalid-property.js
@@ -1,122 +1,122 @@
 export const template = {
-    '$schema': 'https://unpkg.com/@camunda/zeebe-element-templates-json-schema/resources/schema.json',
-    'name': 'AdHoc Template',
-    'id': 'io.camunda.examples.AdHoc.InvalidProperty',
-    'version': 1,
-    'appliesTo': [ 'bpmn:AdHocSubProcess' ],
-    'elementType': { 'value': 'bpmn:AdHocSubProcess' },
-    'properties': [
-        {
-            'type': 'Hidden',
-            'binding': { 'type': 'zeebe:taskDefinition', 'property': 'type' },
-            'value': 'io.camunda.agenticai:aiagent-job-worker:1'
-        },
-        {
-            'type': 'Hidden',
-            'binding': { 'type': 'zeebe:adHoc', 'property': 'youShallNotPass' },
-            'value': 'toolCallResults'
-        },
-        {
-            'type': 'String',
-            'binding': { 'type': 'zeebe:adHoc', 'property': 'outputElement' },
-            'feel': 'required',
-            'value': '={ id: toolCall._meta.id, name: toolCall._meta.name, content: toolCallResult }'
-        }
-    ]
+  '$schema': 'https://unpkg.com/@camunda/zeebe-element-templates-json-schema/resources/schema.json',
+  'name': 'AdHoc Template',
+  'id': 'io.camunda.examples.AdHoc.InvalidProperty',
+  'version': 1,
+  'appliesTo': [ 'bpmn:AdHocSubProcess' ],
+  'elementType': { 'value': 'bpmn:AdHocSubProcess' },
+  'properties': [
+    {
+      'type': 'Hidden',
+      'binding': { 'type': 'zeebe:taskDefinition', 'property': 'type' },
+      'value': 'io.camunda.agenticai:aiagent-job-worker:1'
+    },
+    {
+      'type': 'Hidden',
+      'binding': { 'type': 'zeebe:adHoc', 'property': 'youShallNotPass' },
+      'value': 'toolCallResults'
+    },
+    {
+      'type': 'String',
+      'binding': { 'type': 'zeebe:adHoc', 'property': 'outputElement' },
+      'feel': 'required',
+      'value': '={ id: toolCall._meta.id, name: toolCall._meta.name, content: toolCallResult }'
+    }
+  ]
 };
 
-export const errors =         [
-        {
-            keyword: "const",
-            dataPath: "/properties/0/binding/type",
-            schemaPath: "#/allOf/1/allOf/11/then/properties/properties/contains/properties/binding/properties/type/const",
-            params: {
-                allowedValue: "zeebe:adHoc"
-            },
-            message: "should be equal to constant"
-        },
-        {
-            keyword: "const",
-            dataPath: "/properties/0/binding/property",
-            schemaPath: "#/allOf/1/allOf/11/then/properties/properties/contains/properties/binding/properties/property/const",
-            params: {
-                allowedValue: "outputCollection"
-            },
-            message: "should be equal to constant"
-        },
-        {
-            keyword: "const",
-            dataPath: "/properties/1/binding/property",
-            schemaPath: "#/allOf/1/allOf/11/then/properties/properties/contains/properties/binding/properties/property/const",
-            params: {
-                allowedValue: "outputCollection"
-            },
-            message: "should be equal to constant"
-        },
-        {
-            keyword: "const",
-            dataPath: "/properties/2/binding/property",
-            schemaPath: "#/allOf/1/allOf/11/then/properties/properties/contains/properties/binding/properties/property/const",
-            params: {
-                allowedValue: "outputCollection"
-            },
-            message: "should be equal to constant"
-        },
-        {
-            keyword: "contains",
-            dataPath: "/properties",
-            schemaPath: "#/allOf/1/allOf/11/then/properties/properties/contains",
-            params: {
-                minContains: 1
-            },
-            message: "should contain at least 1 valid item(s)"
-        },
-        {
-            keyword: "if",
-            dataPath: "",
-            schemaPath: "#/allOf/1/allOf/11/if",
-            params: {
-                failingKeyword: "then"
-            },
-            message: "should match \"then\" schema"
-        },
-        {
-            keyword: "enum",
-            dataPath: "/properties/1/binding/property",
-            schemaPath: "#/allOf/1/items/properties/binding/allOf/11/then/properties/property/enum",
-            params: {
-                allowedValues: [
-                    "outputCollection",
-                    "outputElement"
-                ]
-            },
-            message: "should be equal to one of the allowed values"
-        },
-        {
-            keyword: "if",
-            dataPath: "/properties/1/binding",
-            schemaPath: "#/allOf/1/items/properties/binding/allOf/11/if",
-            params: {
-                failingKeyword: "then"
-            },
-            message: "should match \"then\" schema"
-        },
-        {
-            keyword: "type",
-            dataPath: "",
-            schemaPath: "#/oneOf/1/type",
-            params: {
-                type: "array"
-            },
-            message: "should be array"
-        },
-        {
-            keyword: "oneOf",
-            dataPath: "",
-            schemaPath: "#/oneOf",
-            params: {
-                passingSchemas: null
-            },
-            message: "should match exactly one schema in oneOf"
-        }
-    ];
+export const errors = [
+  {
+    keyword: 'const',
+    dataPath: '/properties/0/binding/type',
+    schemaPath: '#/allOf/1/allOf/11/then/properties/properties/contains/properties/binding/properties/type/const',
+    params: {
+      allowedValue: 'zeebe:adHoc'
+    },
+    message: 'should be equal to constant'
+  },
+  {
+    keyword: 'const',
+    dataPath: '/properties/0/binding/property',
+    schemaPath: '#/allOf/1/allOf/11/then/properties/properties/contains/properties/binding/properties/property/const',
+    params: {
+      allowedValue: 'outputCollection'
+    },
+    message: 'should be equal to constant'
+  },
+  {
+    keyword: 'const',
+    dataPath: '/properties/1/binding/property',
+    schemaPath: '#/allOf/1/allOf/11/then/properties/properties/contains/properties/binding/properties/property/const',
+    params: {
+      allowedValue: 'outputCollection'
+    },
+    message: 'should be equal to constant'
+  },
+  {
+    keyword: 'const',
+    dataPath: '/properties/2/binding/property',
+    schemaPath: '#/allOf/1/allOf/11/then/properties/properties/contains/properties/binding/properties/property/const',
+    params: {
+      allowedValue: 'outputCollection'
+    },
+    message: 'should be equal to constant'
+  },
+  {
+    keyword: 'contains',
+    dataPath: '/properties',
+    schemaPath: '#/allOf/1/allOf/11/then/properties/properties/contains',
+    params: {
+      minContains: 1
+    },
+    message: 'should contain at least 1 valid item(s)'
+  },
+  {
+    keyword: 'if',
+    dataPath: '',
+    schemaPath: '#/allOf/1/allOf/11/if',
+    params: {
+      failingKeyword: 'then'
+    },
+    message: 'should match "then" schema'
+  },
+  {
+    keyword: 'enum',
+    dataPath: '/properties/1/binding/property',
+    schemaPath: '#/allOf/1/items/properties/binding/allOf/11/then/properties/property/enum',
+    params: {
+      allowedValues: [
+        'outputCollection',
+        'outputElement'
+      ]
+    },
+    message: 'should be equal to one of the allowed values'
+  },
+  {
+    keyword: 'if',
+    dataPath: '/properties/1/binding',
+    schemaPath: '#/allOf/1/items/properties/binding/allOf/11/if',
+    params: {
+      failingKeyword: 'then'
+    },
+    message: 'should match "then" schema'
+  },
+  {
+    keyword: 'type',
+    dataPath: '',
+    schemaPath: '#/oneOf/1/type',
+    params: {
+      type: 'array'
+    },
+    message: 'should be array'
+  },
+  {
+    keyword: 'oneOf',
+    dataPath: '',
+    schemaPath: '#/oneOf',
+    params: {
+      passingSchemas: null
+    },
+    message: 'should match exactly one schema in oneOf'
+  }
+];
