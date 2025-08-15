b45ba7c3a8e2f6501d1405e2a79bebd7f5208873
Anton von Weltzien <anton.weltzien@camunda.com>
2025-08-15 10:39:43 +0200

    feat: do not allow FEEL on output collection

---
+++ packages/zeebe-element-templates-json-schema/test/fixtures/ad-hoc/invalid-feel-output-collection.js
@@ -0,0 +1,64 @@
+export const template = {
+  '$schema': 'https://unpkg.com/@camunda/zeebe-element-templates-json-schema/resources/schema.json',
+  'name': 'AdHoc Missing Output Collection',
+  'id': 'io.camunda.examples.AdHoc.InvalidFeelOutputCollection',
+  'version': 1,
+  'appliesTo': [ 'bpmn:AdHocSubProcess' ],
+  'elementType': { 'value': 'bpmn:AdHocSubProcess' },
+  'properties': [
+    {
+      'type': 'Hidden',
+      'binding': { 'type': 'zeebe:taskDefinition', 'property': 'type' },
+      'value': 'io.camunda.agenticai:aiagent-job-worker:1'
+    },
+    {
+      'type': 'String',
+      'binding': { 'type': 'zeebe:adHoc', 'property': 'outputCollection' },
+      'feel': 'optional',
+      'value': '={ id: toolCall._meta.id, name: toolCall._meta.name, content: toolCallResult }'
+    },
+    {
+      'type': 'Text',
+      'feel': 'required',
+      'binding': { 'type': 'zeebe:adHoc', 'property': 'outputElement' },
+      'value': '={ id: toolCall._meta.id, name: toolCall._meta.name, content: toolCallResult }'
+    }
+  ]
+};
+
+export const errors = [
+    {
+      keyword: "not",
+      dataPath: "/properties/1",
+      schemaPath: "#/allOf/1/items/allOf/23/allOf/1/then/not",
+      params: {},
+      message: "should NOT be valid"
+    },
+    {
+      keyword: "if",
+      dataPath: "/properties/1",
+      schemaPath: "#/allOf/1/items/allOf/23/allOf/1/if",
+      params: {
+        failingKeyword: "then"
+      },
+      message: "should match \"then\" schema"
+    },
+    {
+      keyword: "type",
+      dataPath: "",
+      schemaPath: "#/oneOf/1/type",
+      params: {
+        type: "array"
+      },
+      message: "should be array"
+    },
+    {
+      keyword: "oneOf",
+      dataPath: "",
+      schemaPath: "#/oneOf",
+      params: {
+        passingSchemas: null
+      },
+      message: "should match exactly one schema in oneOf"
+    }
+  ];
--- packages/zeebe-element-templates-json-schema/test/spec/validationSpec.js
+++ packages/zeebe-element-templates-json-schema/test/spec/validationSpec.js
@@ -646,6 +646,8 @@ describe('validation', function() {
 
       it('ad-hoc/invalid-element-type');
 
+      it('ad-hoc/invalid-feel-output-collection');
+
       it('ad-hoc/invalid-input-type');
 
       it('ad-hoc/invalid-missing-feel-output-element');
