9bc17bae651ae28a5782db78cac58c5182daea3c
Anton von Weltzien <anton.weltzien@camunda.com>
2025-08-18 16:58:44 +0200

    feat: disallow ad-hoc subprocess properties if task definition present
    Related to https://github.com/camunda/tmp-camunda-modeler-adhoc-subprocess/issues/2

---
+++ packages/zeebe-element-templates-json-schema/test/fixtures/ad-hoc/invalid-bpmn-ad-hoc-impl-properties.js
@@ -0,0 +1,142 @@
+export const template = {
+  '$schema': 'https://unpkg.com/@camunda/zeebe-element-templates-json-schema/resources/schema.json',
+  'name': 'AdHoc Template',
+  'id': 'io.camunda.examples.AdHoc.Valid',
+  'description': 'activeElementsCollection, completionCondition, and cancelRemainingInstances cannot be set together with job worker properties',
+  'version': 1,
+  'appliesTo': [ 'bpmn:AdHocSubProcess' ],
+  'elementType': { 'value': 'bpmn:AdHocSubProcess' },
+  'properties': [
+    {
+      'type': 'Hidden',
+      'binding': { 'type': 'zeebe:taskDefinition', 'property': 'type' },
+      'value': 'io.camunda.agenticai:aiagent-job-worker:1'
+    },
+    {
+      'type': 'Hidden',
+      'binding': {
+        'type': 'property',
+        'name': 'cancelRemainingInstances'
+      },
+      'value': 'false'
+    },
+    {
+      'type': 'String',
+      'feel': 'required',
+      'binding': {
+        'type': 'property',
+        'name': 'completionCondition'
+      }
+    },
+    {
+      'type': 'Hidden',
+      'binding': { 'type': 'zeebe:adHoc', 'property': 'activeElementsCollection' },
+      'value': '=anActiveElementsCollection'
+    }
+  ]
+};
+
+export const errors = [
+  {
+    keyword: 'errorMessage',
+    dataPath: '/properties/1',
+    schemaPath: '#/allOf/1/allOf/9/allOf/2/then/allOf/0/properties/properties/items/errorMessage',
+    params: {
+      errors: [
+        {
+          keyword: 'not',
+          dataPath: '/properties/1',
+          schemaPath: '#/allOf/1/allOf/9/allOf/2/then/allOf/0/properties/properties/items/not',
+          params: {},
+          message: 'should NOT be valid',
+          emUsed: true
+        }
+      ]
+    },
+    message: 'Property "cancelRemainingInstances" must not be set when using a "zeebe:taskDefinition" implementation for a "bpmn:AdHocSubProcess"'
+  },
+  {
+    keyword: 'errorMessage',
+    dataPath: '/properties/2',
+    schemaPath: '#/allOf/1/allOf/9/allOf/2/then/allOf/0/properties/properties/items/errorMessage',
+    params: {
+      errors: [
+        {
+          keyword: 'not',
+          dataPath: '/properties/2',
+          schemaPath: '#/allOf/1/allOf/9/allOf/2/then/allOf/0/properties/properties/items/not',
+          params: {},
+          message: 'should NOT be valid',
+          emUsed: true
+        }
+      ]
+    },
+    message: 'Property "completionCondition" must not be set when using a "zeebe:taskDefinition" implementation for a "bpmn:AdHocSubProcess"'
+  },
+  {
+    keyword: 'errorMessage',
+    dataPath: '/properties/3',
+    schemaPath: '#/allOf/1/allOf/9/allOf/2/then/allOf/1/properties/properties/items/errorMessage',
+    params: {
+      errors: [
+        {
+          keyword: 'not',
+          dataPath: '/properties/3',
+          schemaPath: '#/allOf/1/allOf/9/allOf/2/then/allOf/1/properties/properties/items/not',
+          params: {},
+          message: 'should NOT be valid',
+          emUsed: true
+        }
+      ]
+    },
+    message: 'Property "activeElementsCollection" must not be set when using a "zeebe:taskDefinition" implementation for a "bpmn:AdHocSubProcess"'
+  },
+  {
+    keyword: 'if',
+    dataPath: '',
+    schemaPath: '#/allOf/1/allOf/9/allOf/2/if',
+    params: {
+      failingKeyword: 'then'
+    },
+    message: 'should match "then" schema'
+  },
+  {
+    keyword: 'enum',
+    dataPath: '/properties/3/binding/property',
+    schemaPath: '#/allOf/1/items/properties/binding/allOf/11/then/properties/property/enum',
+    params: {
+      allowedValues: [
+        'outputCollection',
+        'outputElement'
+      ]
+    },
+    message: 'should be equal to one of the allowed values'
+  },
+  {
+    keyword: 'if',
+    dataPath: '/properties/3/binding',
+    schemaPath: '#/allOf/1/items/properties/binding/allOf/11/if',
+    params: {
+      failingKeyword: 'then'
+    },
+    message: 'should match "then" schema'
+  },
+  {
+    keyword: 'type',
+    dataPath: '',
+    schemaPath: '#/oneOf/1/type',
+    params: {
+      type: 'array'
+    },
+    message: 'should be array'
+  },
+  {
+    keyword: 'oneOf',
+    dataPath: '',
+    schemaPath: '#/oneOf',
+    params: {
+      passingSchemas: null
+    },
+    message: 'should match exactly one schema in oneOf'
+  }
+];
