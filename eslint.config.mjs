a0e84d4433a7ef72e694cb04746a0299160c32df
Nico Rehwaldt <nico.rehwaldt@camunda.com>
2026-06-29 16:42:44 +0200

    chore: lint all projects

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,28 +1,42 @@
 import bpmnIoPlugin from 'eslint-plugin-bpmn-io';
 
+const files = {
+  ignored: [
+    '*.json',
+    '.nyc_output',
+    'coverage',
+    '**/dist'
+  ],
+  build: [
+    '**/*.js',
+    '**/*.mjs'
+  ],
+  test: [
+    '**/test/**/*.js'
+  ]
+};
+
+
 export default [
   {
-    ignores: [
-      '*.json',
-      '.nyc_output',
-      'coverage',
-      'dist'
-    ]
+    ignores: files.ignored
   },
   ...bpmnIoPlugin.configs.node.map(config => {
     return {
       ...config,
-      files: [
-        '**/*.js'
-      ]
+      files: files.build
     };
   }),
+  {
+    languageOptions: {
+      ecmaVersion: 2026
+    },
+    files: files.build
+  },
   ...bpmnIoPlugin.configs.mocha.map(config => {
     return {
       ...config,
-      files: [
-        '**/test/**/*.js'
-      ]
+      files: files.test
     };
   })
 ];
